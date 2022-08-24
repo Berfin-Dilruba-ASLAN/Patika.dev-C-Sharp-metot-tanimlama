75817f3637a113fa201bac95da20e044073eb60a
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-08-24 13:42:26 +0300

    Add files via upload

---
+++ metot_tanımlama/metot_tanimlama/metot_tanimlama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace metot_tanimlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Degistir(ref x, ref y);
+            Console.WriteLine("X = " + x.ToString());
+            Console.WriteLine("Y = " + y.ToString());
+        }
+        static void Degistir(ref int x , ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
